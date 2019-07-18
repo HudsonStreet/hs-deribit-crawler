5919e1b45571fd65b309872435603e6f4d0235af
jinsiang2008@gmail.com <sjin@hioscar.com>
2019-07-18 00:15:09 -0400

    Split client out

--- pull_data.py
+++ pull_data.py
@@ -37,13 +37,10 @@ def read_config(config_path, section):
         raise Exception('section {0} not found in the {1} file'.format(section, abs_config_path))
         
     return info
-
-def _load_data(credentials):
+#TODO(jinsiang): Update to use request sessions
+def _deribit_api_client(credentials):
     """
-    Load data by using debrit API
-    Need to put the openapi_client python in parent folder
-    :param credentials: dic with key and secret information
-    :return return the required data
+    Take creds and return api client
     """
     conf = openapi_client.Configuration()
     client = openapi_client.api_client.ApiClient(conf)
@@ -56,11 +53,46 @@ def _load_data(credentials):
     client_authed =  openapi_client.api_client.ApiClient(conf_authed)
     # Create an instance of the market api class
     market_api_instance = openapi_client.api.market_data_api.MarketDataApi(client_authed)
-    currency = 'BTC'
-    kind = 'option'
+
+    return market_api_instance
+
+def _load_data(credentials, base_currency, derivative_kind):
+    """
+    Load data by using debrit API
+    Need to put the openapi_client python in parent folder
+    :param credentials: dic with key and secret information
+    :return return the required data
+    e.g.
+    {
+    'volume': 14.6,
+    'underlying_price': 9548.31,
+    'underlying_index': 'BTC-27DEC19',
+    'quote_currency': 'USD',
+    'open_interest': 83.4,
+    'mid_price': 0.441,
+    'mark_price': 0.44181073,
+    'low': 0.3885,
+    'last': 0.4295,
+    'interest_rate': 0.0,
+    'instrument_name': 'BTC-27DEC19-12000-P',
+    'high': 0.4565,
+    'estimated_delivery_price': 9484.63,
+    'creation_timestamp': 1563340045235,
+    'bid_price': 0.434,
+    'base_currency': 'BTC',
+    'ask_price': 0.448
+    }
+    """
+    # Create an instance of the market api class
+    market_api_instance = _deribit_api_client(credentials)
+    currency = base_currency #'BTC'
+    kind = derivative_kind #'option'
     api_response = market_api_instance.public_get_book_summary_by_currency_get(currency, kind=kind)
+    print(api_response['result'])
+    
     return api_response['result']
 
+#TODO(jinsiang): Update the writing pattern, this might causing memLeak
 def _transform_data(data):
     final_data = [] 
     #  Generate a table as below format:
@@ -90,7 +122,7 @@ def save_csv(data):
 
 if __name__ == "__main__":
     credentials = read_config('/config_prod.ini', 'deribit_credential')
-    data = _load_data(credentials)
+    data = _load_data(credentials, 'BTC', 'option')
     final_data = _transform_data(data)
     save_csv(final_data)
 
