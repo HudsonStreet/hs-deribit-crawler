e206ec7b72dab2c11d48727919f831826d66f9a7
YUJIE REN <irene@YUJIEs-MacBook-Pro.local>
2019-09-08 19:53:16 -0400

    new commit based on comments

--- pull_data.py
+++ pull_data.py
@@ -13,6 +13,7 @@ from pprint import pprint
 lib_path = os.path.abspath(os.path.join('.','python_deribit'))
 sys.path.append(lib_path)
 import openapi_client
+_market_api_instance = None
 
 def read_config(config_path, section):
     """
@@ -42,19 +43,22 @@ def _deribit_api_client(credentials):
     """
     Take creds and return api client
     """
-    conf = openapi_client.Configuration()
-    client = openapi_client.api_client.ApiClient(conf)
-    publicApi = openapi_client.api.public_api.PublicApi(client)
-    response = publicApi.public_auth_get('client_credentials', '', '', credentials['auth_key'], credentials['auth_secret'], '', '', '', scope='session:test wallet:read')
-    access_token = response['result']['access_token']
-    # Set up token information
-    conf_authed = openapi_client.Configuration()
-    conf_authed.access_token = access_token
-    client_authed =  openapi_client.api_client.ApiClient(conf_authed)
-    # Create an instance of the market api class
-    market_api_instance = openapi_client.api.market_data_api.MarketDataApi(client_authed)
+    global _market_api_instance
+    if _market_api_instance == None:
 
-    return market_api_instance
+        conf = openapi_client.Configuration()
+        client = openapi_client.api_client.ApiClient(conf)
+        publicApi = openapi_client.api.public_api.PublicApi(client)
+        response = publicApi.public_auth_get('client_credentials', '', '', credentials['auth_key'], credentials['auth_secret'], '', '', '', scope='session:test wallet:read')
+        access_token = response['result']['access_token']
+        # Set up token information
+        conf_authed = openapi_client.Configuration()
+        conf_authed.access_token = access_token
+        client_authed =  openapi_client.api_client.ApiClient(conf_authed)
+        # Create an instance of the market api class
+        _market_api_instance = openapi_client.api.market_data_api.MarketDataApi(client_authed)
+
+    return _market_api_instance
 #get bid/ask size for each instrument
 def _data_get_size_by_instrument(api_client_instance, instrument):
     bid_ask_api_response = api_client_instance.public_get_order_book_get(instrument)
@@ -74,7 +78,6 @@ def _data_get_size_by_instrument(api_client_instance, instrument):
     return bid_size_by_instrument, ask_size_by_instrument
 
 def _load_data(credentials, base_currency, derivative_kind):
-    global market_api_instance
     """
     Load data by using debrit API
     Need to put the openapi_client python in parent folder
@@ -111,15 +114,14 @@ def _load_data(credentials, base_currency, derivative_kind):
     return api_response['result']
 
 #TODO(jinsiang): Update the writing pattern, this might causing memLeak
-def _transform_data(data):
-    global market_api_instance
+def _transform_data(data, credentials):
     final_data = [] 
     #  Generate a table as below format:
     # 'RowID', 'Date', 'volume', 'bid', 'last', 'ask', 'volume', '
     #  %change', 'change','strike'
     #authetication and create api_instance
     #tmp_crend = read_config('/config_prod.ini', 'deribit_credential')
-    #tmp_api = _deribit_api_client(tmp_crend)
+    tmp_api = _deribit_api_client(credentials)
     for ins in data:
         new_obj = {}
         new_obj['instrument_name'] = ins['instrument_name']
@@ -135,12 +137,12 @@ def _transform_data(data):
         new_obj['volume'] = ins['volume']
         new_obj['underlying_price'] = ins['underlying_price']
         #last price
-        new_obj['last_price']=ins['last']
+        new_obj['last_price'] = ins['last']
         #get bid/ask size
-        bid_ask = _data_get_size_by_instrument(market_api_instance, ins['instrument_name'])
+        bid_ask = _data_get_size_by_instrument(tmp_api, ins['instrument_name'])
         #print("bid_ask",bid_ask)
-        new_obj['bid_size']=bid_ask[0]
-        new_obj['ask_size']=bid_ask[1]
+        new_obj['bid_size'] = bid_ask[0]
+        new_obj['ask_size'] = bid_ask[1]
         final_data.append(new_obj)
 
     return final_data
@@ -150,12 +152,12 @@ def save_csv(data):
     #'creation_time_c','expiry_date_c','buy_volume_c','bid_price_c','last_price_c','ask_price_c','strike_price_c','sale_volume_c','underlying_price_c',
     #'creation_time_p','expiry_date_p','buy_volume_p','bid_price_p','last_price_p','ask_price_p','strike_price_p','sale_volume_p','underlying_price_p',
     #column name list
-    col_lis =['creation_time_c','expiry_date_c','bid_size_c','bid_price_c','last_price_c','ask_price_c','strike_price_c','ask_size_c','volume_c','underlying_price_c',
+    col_lis = ['creation_time_c','expiry_date_c','bid_size_c','bid_price_c','last_price_c','ask_price_c','strike_price_c','ask_size_c','volume_c','underlying_price_c',
     'creation_time_p','expiry_date_p','bid_size_p','bid_price_p','last_price_p','ask_price_p','strike_price_p','ask_size_p','volume_p','underlying_price_p']
     call_data = [] #call dataframe
     put_data = [] #put dataframe
     for it in data:
-        if it['option_type']=='C':
+        if it['option_type'] == 'C':
             call_data.append(it)
             #print("instrument_name:",it['instrument_name'])
         else:
@@ -168,18 +170,18 @@ def save_csv(data):
     pc_df = pd.DataFrame()
     for col_it in col_lis:
         if '_c' in col_it:
-            pc_df[col_it]=call_df[col_it[:-2]]
+            pc_df[col_it] = call_df[col_it[:-2]]
         else:
-            pc_df[col_it]=put_df[col_it[:-2]]
+            pc_df[col_it] = put_df[col_it[:-2]]
     #output
-    pc_df.to_csv('formated_data_pc.csv',index=False)
+    pc_df.to_csv('formated_data_pc.csv',index = False)
     #df = pd.DataFrame(data)
-    #df.to_csv(r'final_data.csv',index=False)
+    #df.to_csv(r'final_data.csv',index = False)
 
 if __name__ == "__main__":
     credentials = read_config('/config_prod.ini', 'deribit_credential')
     data = _load_data(credentials, 'BTC', 'option')
-    final_data = _transform_data(data)
+    final_data = _transform_data(data, credentials)
     save_csv(final_data)
 
 
